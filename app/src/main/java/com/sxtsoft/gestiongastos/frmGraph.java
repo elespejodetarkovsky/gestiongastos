81ea2125367d18da3e9c318e2f70a2e93324ba5a
elespejodetarkovsky <sebastian.turone@gmail.com>
2019-08-30 13:43:56 +0200

    se pasa a barchart

--- app/src/main/java/com/sxtsoft/gestiongastos/frmGraph.java
+++ app/src/main/java/com/sxtsoft/gestiongastos/frmGraph.java
@@ -6,17 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class frmGraph extends AppCompatActivity {
 
@@ -25,7 +31,8 @@ public class frmGraph extends AppCompatActivity {
     private float[] yData = {25,3f, 10,6f, 44,31f, 53,4f};
     private String[] xData = {"SUMINISTROS","SUMINISTROS2","SUMINISTROS3","TRANSPORTES"};
     private Description description;
-    private PieChart pieChart;
+    private BarChart barChart;
+
 
 
     @Override
@@ -33,35 +40,59 @@ public class frmGraph extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frm_graph);
 
-        pieChart = (PieChart) findViewById(R.id.chartPie);
+        //pieChart = (PieChart) findViewById(R.id.chartPie);
+        barChart = (BarChart) findViewById(R.id.chartBar);
 
         description = new Description();
 
         description.setText("Gastos por suministro");
 
-        pieChart.setDescription(description);
-        pieChart.setRotationEnabled(true);
-        pieChart.setHoleRadius(25f);
-        pieChart.setTransparentCircleAlpha(0);
-        pieChart.setCenterText("center planet");
-        pieChart.setCenterTextSize(10);
-        pieChart.setDrawEntryLabels(true);
+        //CREAMOS LA LISTA CON LOS VALORES DE ENTRADA
+        List<BarEntry> entradas = new ArrayList<>();
+
+        entradas.add(new BarEntry(0f,2));
+        entradas.add(new BarEntry(1f,4));
+        entradas.add(new BarEntry(2f, 6));
+        entradas.add(new BarEntry(3f, 8));
+        entradas.add(new BarEntry(4f,3));
+        entradas.add(new BarEntry(5f, 1));
 
 
-        addDataSet();
+        //MANDAMOS LOS DATOS PARA CREAR LA GRAFICA
+        BarDataSet datos = new BarDataSet(entradas,"grafico barras");
 
-        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
-            @Override
-            public void onValueSelected(Entry e, Highlight h) {
-                Log.d(TAG, e.toString());
-                Log.d(TAG, h.toString());
-            }
+        BarData data = new BarData(datos);
 
-            @Override
-            public void onNothingSelected() {
+        //PONEMOS COLOR A CADA BARRA
+        datos.setColors(ColorTemplate.COLORFUL_COLORS);
 
-            }
-        });
+        //SEPARACION ENTRE BARRAS
+        data.setBarWidth(0.9f);
+
+        barChart.setData(data);
+
+        //PONE LAS BARRAS CENTRADAS
+        barChart.setFitBars(true);
+
+        barChart.invalidate(); //hace refresh
+
+
+
+
+//        addDataSet();
+//
+//        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+//            @Override
+//            public void onValueSelected(Entry e, Highlight h) {
+//                Log.d(TAG, e.toString());
+//                Log.d(TAG, h.toString());
+//            }
+//
+//            @Override
+//            public void onNothingSelected() {
+//
+//            }
+//        });
 
     }
 
@@ -93,17 +124,17 @@ public class frmGraph extends AppCompatActivity {
         colors.add(Color.YELLOW);
         colors.add(Color.MAGENTA);
 
-        pieDataSet.setColors(colors);
-
-        //agregar etiquetas al grafico
-        Legend legend = pieChart.getLegend();
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
-
-        //creo pie data object
-        PieData pieData = new PieData(pieDataSet);
-        pieChart.setData(pieData);
-        pieChart.invalidate();
+//        pieDataSet.setColors(colors);
+//
+//        //agregar etiquetas al grafico
+//        Legend legend = pieChart.getLegend();
+//        legend.setForm(Legend.LegendForm.CIRCLE);
+//        legend.setPosition(Legend.LegendPosition.LEFT_OF_CHART);
+//
+//        //creo pie data object
+//        PieData pieData = new PieData(pieDataSet);
+//        pieChart.setData(pieData);
+//        pieChart.invalidate();
 
 
     }
